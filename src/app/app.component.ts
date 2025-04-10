857c05d5687801e8172cfa2c08103e38c89c64ec
Tanya Chauhan <tanuuchauhan2000@gmail.com>
2025-04-10 22:41:54 +0530

    UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactUsComponent } from './homepage/contact-us/contact-us.component';
+import { AllProductsComponent } from './all-products/all-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        loadComponent:()=>HomepageComponent,
+    },
+    {
+        path:'signup',
+        loadComponent:()=>SignupComponent
+    },
+    {
+        path:'login',
+        loadComponent:()=>LoginComponent
+    },
+    {
+        path:'contact-us',
+        loadComponent:()=>ContactUsComponent
+    },
+    {
+        path:'products',
+        loadComponent:()=>AllProductsComponent
+    }
+];
